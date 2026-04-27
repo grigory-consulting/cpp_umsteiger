a7b3f99bae0a4422522203d5e81cbbdc9ecca3d9
grigory-consulting <consulting@vadze.de>
2026-04-27 11:24:31 +0200

    01_grundlagen.cpp

---
+++ 01_grundlagen.cpp
@@ -0,0 +1,28 @@
+
+
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+
+
+namespace variablen {
+
+void demo()
+{
+    std::cout << "Hi";
+}
+
+} // namespace variablen
+
+
+// -----------------------------------------------------------------------------
+int main()
+{
+
+    variablen::demo();
+
+
+}
