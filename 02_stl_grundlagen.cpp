d8ea9cf698a1d784b4a7b26b979ed9ff33305b25
grigory-consulting <consulting@vadze.de>
2026-04-27 13:30:57 +0200

    02_stl_grundlagen.cpp

---
+++ 02_stl_grundlagen.cpp
@@ -0,0 +1,33 @@
+
+
+#include <algorithm>
+#include <deque>
+#include <forward_list>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+
+namespace container {
+
+void demo()
+{
+
+
+}
+
+}
+
+int main()
+{
+    std::cout << "=== Container ===\n";
+    container::demo();
+
+
+}
